7d6e990dce29347eb1a8904a68654e8a365e8fed
AndreiBoghean <67372068+AndreiBoghean@users.noreply.github.com>
2025-02-08 16:31:43 +0000

    Create installPackages.sh

---
+++ setup_scripts/installPackages.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+cd $(dirname $(readlink -f $0))/.. # move into InfiniTime folder, relative to this script's location in InfiniTime/setup_scripts
+# ^ needed since pip below uses a relative link for reqs.txt
+
+pacman -Sy npm base-devel cmake unzip
+pip install --break-system-packages -r tools/mcuboot/requirements.txt
+pip install --break-system-packages adafruit-nrfutil
