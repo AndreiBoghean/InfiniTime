5b55423935d69fb4e935b4ddace6e90dc00270cd
AndreiBoghean <67372068+AndreiBoghean@users.noreply.github.com>
2025-02-08 13:30:23 +0000

    remove infiniemu download and add note in setupOSrepositories.sh

--- setup_scripts/setupOSrepositories.sh
+++ setup_scripts/setupOSrepositories.sh
@@ -4,16 +4,10 @@
 cd $(dirname $(readlink -f $0))/.. # move into InfiniTime folder, relative to this script's location in InfiniTime/setup_scripts
 # ^ assume the script is being executed from some arbitrary path.
 
-# # download infinitime
-# git clone git@github.com:AndreiBoghean/InfiniTime.git
-
 npm install lv_font_conv
 
 mkdir infi_libs
 cd infi_libs
-
-# clone the emulator for ininitime
-git clone git@github.com:pipe01/InfiniEmu.git
 
 # gcc arm compiler for linux x86-64 from https://developer.arm.com/downloads/-/gnu-rm/10-3-2021-10
 url='https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2?rev=78196d3461ba4c9089a67b5f33edf82a&hash=D484B37FF37D6FC3597EBE2877FB666A41D5253B'
@@ -26,3 +20,8 @@ url='https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v15.x.x/nRF5_SDK_15.3.0_
 filename='nRF5_SDK_15.3.0_59ac345.zip'
 wget $url -O $filename
 unzip $filename
+
+# clone the emulator for ininitime
+# git clone git@github.com:pipe01/InfiniEmu.git
+echo "dependency downloading should now be complete and present within $(pwd)"
+echo "note: to be able to use the emulator with the provided build script, please download the latest infiniemu build from https://github.com/pipe01/InfiniEmu/ and place it's contents within $(pwd)/infiniemu"
