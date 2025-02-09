aa8a97df0a25e106f07bc7ebbddda51abba82d68
AndreiBoghean <67372068+AndreiBoghean@users.noreply.github.com>
2025-02-09 14:31:09 +0000

    remove needless readlinks and change repo cloning method ssh->https in installGlime.sh

--- glimeSupport/installGlime.sh
+++ glimeSupport/installGlime.sh
@@ -8,7 +8,7 @@ infi=$1
 glime=$2
 
 if [ -f $infi ]; then infi="$(dirname $(readlink -f $0))/.."; fi
-if [ -f $glime ]; then glime=$infi/glimeSupport/glime; git clone git@github.com:AndreiBoghean/glime.git $glime; fi
+if [ -f $glime ]; then glime=$infi/glimeSupport/glime; git clone https://github.com/AndreiBoghean/glime.git $glime; fi
 
 infi=$(readlink $infi -f)
 glime=$(readlink $glime -f)
@@ -22,18 +22,18 @@ cd $infi
 
 # insert the wrapper app which acts as the "barrier" between OS and external app.
 # the wrapper app calls the main() of the external app.
-ln -v -s $(readlink $infi/glimeSupport/wrapApp.cpp -f) $infi/src/displayapp/screens/wrapApp.cpp
-ln -v $(readlink $infi/glimeSupport/wrapApp.h -f) $infi/src/displayapp/screens/wrapApp.h # using hardlink here because header includes seem to mess up with soft links.
+ln -v -s $infi/glimeSupport/wrapApp.cpp $infi/src/displayapp/screens/wrapApp.cpp
+ln -v $infi/glimeSupport/wrapApp.h $infi/src/displayapp/screens/wrapApp.h # using hardlink here because header includes seem to mess up with soft links.
 
 # the external app that is OS-agnostic.
-ln -v -s $(readlink $glime/externApp.cpp -f) $infi/src/displayapp/screens/externApp.cpp
-ln -v -s $(readlink $glime/externApp.h -f) $infi/src/displayapp/screens/externApp.h
+ln -v -s $glime/externApp.cpp $infi/src/displayapp/screens/externApp.cpp
+ln -v -s $glime/externApp.h $infi/src/displayapp/screens/externApp.h
 
 # the InfiniTime-specific implementation for the compatability library
-ln -v -s $(readlink $infi/glimeSupport/compat.cpp -f) $infi/src/compat.cpp
-ln -v -s $(readlink $glime/compat.h -f) $infi/src/compat.h
+ln -v -s $infi/glimeSupport/compat.cpp $infi/src/compat.cpp
+ln -v -s $glime/compat.h $infi/src/compat.h
 
 # the Infinitime system component that provides services to the compatability implementation such as background timers.
 mkdir -p $infi/src/components/compatProvider
-ln -v -s $(readlink $infi/glimeSupport/compatProvider.cpp -f) $infi/src/components/compatProvider/compatProvider.cpp
-ln -v -s $(readlink $infi/glimeSupport/compatProvider.h -f) $infi/src/components/compatProvider/compatProvider.h
+ln -v -s $infi/glimeSupport/compatProvider.cpp $infi/src/components/compatProvider/compatProvider.cpp
+ln -v -s $infi/glimeSupport/compatProvider.h $infi/src/components/compatProvider/compatProvider.h
